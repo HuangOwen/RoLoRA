6f92f5fabc3ab99a00dbfcf6e2575d09dbb64c78
Xijie Huang <huangxijie1108@gmail.com>
2024-09-22 03:29:17 +0000

    update codes

--- merge_lora.sh
+++
@@ -1,9 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python src/export.py \
---model_name_or_path meta-llama/Llama-2-7b-hf \
---adapter_name_or_path saves/llama2-7b-lora/ \
---template default \
---finetuning_type lora \
---export_dir models/llama2-7b-lora \
---export_size 2 \
---export_device cpu \
---export_legacy_format false 
